222bffcc8af7d0707539495911397607afa48fa1
dev-avi09 <avianant09@gmail.com>
2026-02-28 22:26:36 +0530

    Added Selection Sort and Insertion Sort

--- app.py
+++ app.py
@@ -22,6 +22,29 @@ def sort():
                     arr[j], arr[j+1] = arr[j+1], arr[j]
                 steps.append(arr.copy())
 
+    elif algorithm == 'selection':
+        arr = array.copy()
+        n = len(arr)
+        for i in range(n):
+            min_idx = i
+            for j in range(i+1, n):
+                if arr[j] < arr[min_idx]:
+                    min_idx = j
+            arr[i], arr[min_idx] = arr[min_idx], arr[i]
+            steps.append(arr.copy())
+
+    elif algorithm == 'insertion':
+        arr = array.copy()
+        for i in range(1, len(arr)):
+            key = arr[i]
+            j = i - 1
+            while j >= 0 and arr[j] > key:
+                arr[j+1] = arr[j]
+                j -= 1
+                steps.append(arr.copy())
+            arr[j+1] = key
+            steps.append(arr.copy())
+
     return jsonify({'steps': steps})
 
 if __name__ == '__main__':
